301e7c1bacedbdd58e2454b0671198c52b442eed
Yifei Wang <hello.yifei@icloud.com>
2016-11-21 21:28:50 +0100

    add event listener on each button element

--- lab3.js
+++ lab3.js
@@ -1,25 +1,30 @@
 window.addEventListener('load', function() {
 	console.log(event.target)
-	let b = document.getElementById('menu');
-	let l = document.getElementById('lis');
-	l.style.display = 'none';
-	//b.onclick = function() {	
-	//}
-	b.addEventListener('click',function(event){	
-		if(l.style.display=='none'){
-			l.style.display='block';
+	let buttonMenu = document.getElementById('menu');
+	let drawOptions = document.getElementById('drawOptions');
+	drawOptions.style.display = 'none';
+	
+	buttonMenu.addEventListener('click',function(event){	
+		if(drawOptions.style.display=='none'){
+			drawOptions.style.display='block';
 		}else{
-			l.style.display='none';
+			drawOptions.style.display='none';
 		}
 	});
+
+	let drawArray = document.getElementsByClassName("draw");
 	
+	for(let i=0; i<drawArray.length; i++){
+		drawArray[i].addEventListener('click', function(event){
+			let selectedOption = event.target.innerHTML;
+			let status = document.getElementById('status');
+			status.innerHTML = selectedOption;
+		})
+	}
+			
 	let canvas = document.getElementById("myCanvas");
 	let context = canvas.getContext('2d');
-
 	let status = document.getElementById('status');
-	//window.addEventListener('click', function(event){
-		
-	//})
 
 
 })
@@ -29,9 +34,4 @@ function changeColor(){
 	i.value = colorInput.value;
 	let status = document.getElementById('status');
 	status.innerHTML = 'You picked color with '+  i.value;
-}
-function selectedOption(event){
-	let selectedOption = event.target.innerHTML;
-	let status = document.getElementById('status');
-	status.innerHTML = selectedOption;
 }
